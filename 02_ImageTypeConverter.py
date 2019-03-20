bcd0c739830b524e0caffe986e328e895195fcc2
Shahid1993 <31109285+Shahid1993@users.noreply.github.com>
2019-03-20 11:38:47 +0530

    Update 02_ImageTypeConverter.py

--- 02_ImageTypeConverter.py
+++ 02_ImageTypeConverter.py
@@ -1,1 +1,15 @@
+### Based on https://medium.freecodecamp.org/how-to-build-an-image-type-convertor-in-six-lines-of-python-d63c3c33d1db
 
+# PIL : Python Image Library
+# Pillow : supports even > Python 3.0.
+# pip3 install Pillow
+
+
+from PIL import Image  # Python Image Library - Image Processing
+import glob            # for iterating through files of the given folder in the OS
+#print(glob.glob("*.png"))
+# based on SO Answer: https://stackoverflow.com/a/43258974/5086335
+for file in glob.glob("*.png"):
+    im = Image.open(file)
+    rgb_im = im.convert('RGB')
+rgb_im.save(file.replace("png", "jpg"), quality=95)
